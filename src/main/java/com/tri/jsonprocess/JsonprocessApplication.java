cce0a9cd17613f0b00194c003aaca19ffcfc57ba
Tri Nguyen <tri.nguyen.jobs@gmail.com>
2023-06-03 01:51:03 -0500

    first version

---
+++ src/main/java/com/tri/jsonprocess/JsonprocessApplication.java
@@ -0,0 +1,13 @@
+package com.tri.jsonprocess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonprocessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonprocessApplication.class, args);
+	}
+
+}
